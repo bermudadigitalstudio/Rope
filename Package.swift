0581106f07565cfa9731ddaee5f9621b7ea39ee3
Johannes Erhardt <johannes@johanneserhardt.com>
2017-01-12 21:59:26 +0100

    Includes the „RopeLibpq“ package as dependency.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "Rope"
+    name: "Rope",
+    dependencies: [
+        .Package(url: "https://github.com/bermudadigitalstudio/rope-libpq.git", majorVersion: 0, minor: 1)
+    ]
 )
