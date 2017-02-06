012b476964fbf8b4ec140b4899062a1931dc54a2
Johannes <johannes@johanneserhardt.com>
2017-02-06 13:24:20 +0100

    #39 Includes tests for type conversion of type `json`.

--- Tests/RopeTests/RopeQueryJSONTests.swift
+++ Tests/RopeTests/RopeQueryJSONTests.swift
@@ -5,6 +5,8 @@ final class RopeQueryJSONTests: XCTestCase {
     let creds = TestCredentials.getCredentials()
     var conn: Rope? // auto-tested optional db connection
     
+    let insertQuery = "INSERT INTO json (json) VALUES ('{\"due_in_seconds\":0,\"method\":\"POST\",\"headers\":{},\"url\":\"http://localhost\"}') RETURNING id;"
+    
     override func setUp() {
         super.setUp()
         
@@ -26,7 +28,7 @@ final class RopeQueryJSONTests: XCTestCase {
     }
     
     func testQueryInsertStatement() {
-        guard let res = try? conn?.query("INSERT INTO json (json) VALUES ('{\"due_in_seconds\":0,\"method\":\"POST\",\"headers\":{},\"url\":\"http://localhost\"}') RETURNING id;") else {
+        guard let res = try? conn?.query(insertQuery) else {
             XCTFail("res should not be nil"); return
         }
         XCTAssertEqual(res?.rows().count, 1)
@@ -38,5 +40,31 @@ final class RopeQueryJSONTests: XCTestCase {
         let id = row["id"] as? Int
         XCTAssertNotNil(id)
         XCTAssertEqual(id, 1)
+    }
+    
+    func testQuerySelectStatement() {
+        guard let _ = try? conn?.query(insertQuery),
+            let select = try? conn?.query("SELECT *  FROM json")
+        else {
+            XCTFail("res should not be nil"); return
+        }
+        
+        guard let row = select?.rows().first else {
+            XCTFail("res should not be nil"); return
+        }
+        
+        let payload = row["json"] as? [String: Any?]
+        XCTAssertNotNil(payload)
+        
+        guard let method = payload?["method"] as? String,
+            let dueInSeconds = payload?["due_in_seconds"] as? Int,
+            let urlString = payload?["url"] as? String
+        else {
+            XCTFail("res should not be nil"); return
+        }
+        
+        XCTAssertEqual(urlString, "http://localhost")
+        XCTAssertEqual(method, "POST")
+        XCTAssertEqual(dueInSeconds, 0)
     }
 }
