9655f0df9e9457c199361832549f10e66ce84378
Johannes <johannes@johanneserhardt.com>
2017-02-20 19:56:32 +0100

    #66 Tests multi queries.

--- Tests/RopeTests/RopeQueryTests.swift
+++ Tests/RopeTests/RopeQueryTests.swift
@@ -24,13 +24,14 @@ final class RopeQueryTests: XCTestCase {
         }
 
         // create a table with different types as test, payload can be nil
-        var sql = "CREATE TABLE rope (id SERIAL PRIMARY KEY, my_text TEXT, my_bool BOOLEAN default FALSE"
-        sql += ", my_varchar VARCHAR(3) default 'abc', my_char CHAR(1) default 'x', my_null_text TEXT default null"
-        sql += ", my_real REAL default 123.456, my_double DOUBLE PRECISION default 456.789"
-        // sql += ", row_to_json(row(1,'foo'))"
-        sql += ", my_date DATE default (now() at time zone 'utc')"
-        sql += ", my_ts TIMESTAMP default (now() at time zone 'utc')"
-        sql += ");"
+        let sql = String(
+            "CREATE TABLE rope (id SERIAL PRIMARY KEY, my_text TEXT, my_bool BOOLEAN default FALSE,",
+            "my_varchar VARCHAR(3) default 'abc', my_char CHAR(1) default 'x', my_null_text TEXT default null,",
+            "my_real REAL default 123.456, my_double DOUBLE PRECISION default 456.789,",
+            "my_date DATE default (now() at time zone 'utc'),",
+            "my_ts TIMESTAMP default (now() at time zone 'utc'));"
+        )
+        
         guard let _ = try? db.query(sql) else {
             XCTFail("_ should not be nil")
             return
@@ -269,6 +270,17 @@ final class RopeQueryTests: XCTestCase {
         }
 
         XCTAssertNotEqual(getLatestUserName(), "Black hat hacker")
+    }
+    
+    func testMultiQueries() {
+        let sql = String(
+            "CREATE TEMPORARY TABLE multi_queries_test(id SERIAL PRIMARY KEY, name text);",
+            "INSERT INTO users(name) VALUES ('Sebastian'),('Thomas'),('Johannes'),('Gabriel');"
+        )
+        
+        XCTAssertThrowsError(
+            try conn?.query(sql)
+        )
     }
 
     /// helper function which tests the connection and
