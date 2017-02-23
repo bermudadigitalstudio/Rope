3d2ae37bbe622c2c53485b8fb5ce5c7ed60da1c3
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2017-02-23 12:30:36 +0100

    fixed tests

--- Tests/RopeTests/RopeInjectionsTests.swift
+++ Tests/RopeTests/RopeInjectionsTests.swift
@@ -17,6 +17,7 @@ final class RopeInjectionTests: XCTestCase {
         conn = db
     }
 
+    /// demo under what circumstances an SQL injection is possible
     func testSQLInjection() {
         let seedStatement = "CREATE TEMPORARY TABLE users(id SERIAL PRIMARY KEY, name text); "
         let sql = "INSERT INTO users(name) VALUES ('Sebastian'),('Thomas'),('Johannes'),('Gabriel')"
@@ -26,29 +27,32 @@ final class RopeInjectionTests: XCTestCase {
                 XCTFail("res should not be nil"); return
         }
 
+        // query() without params is considered to be dangerous - so handle with care!
         let maliciousInput = "lol'; INSERT INTO users(name) VALUES ('Black hat hacker') --"
 
-        XCTAssertThrowsError(
-            try conn?.query("SELECT * FROM users WHERE name = '\(maliciousInput)'")
-        )
-
-        func getLatestUserName() -> String? {
-            guard let users = try? conn?.query("SELECT * FROM users") else {
-                return nil
-            }
-            let latestUser = users?.rows().last?["name"] as? String
-            return latestUser
+        guard let _ = try? conn?.query("SELECT * FROM users WHERE name = '\(maliciousInput)'") else {
+                XCTFail("res should not be nil"); return
         }
 
-        XCTAssertNotEqual(getLatestUserName(), "Black hat hacker")
+        // oh no, the hacker took over!!
+        XCTAssertEqual(getLatestUserName(), "Black hat hacker")
 
-        // Reset and try again
+        // now use query() with params - it is safe
         guard let _ = try? conn?.query("DISCARD TEMPORARY"),
             let _ = try? conn?.query(seedStatement),
             let _ = try? conn?.query("SELECT * FROM users WHERE name = $1", params: [maliciousInput]) else {
                 XCTFail("res should not be nil"); return
         }
-
+        // we kept the hacker out
         XCTAssertNotEqual(getLatestUserName(), "Black hat hacker")
+    }
+
+    // helper function
+    func getLatestUserName() -> String? {
+        guard let users = try? conn?.query("SELECT * FROM users") else {
+            return nil
+        }
+        let latestUser = users?.rows().last?["name"] as? String
+        return latestUser
     }
 }
